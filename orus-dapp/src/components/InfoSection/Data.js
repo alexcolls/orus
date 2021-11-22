a7524d71d7c03547569c1b8d04ce243aabe6e93b
quantium-rock <alexcollsoutumuro@gmail.com>
2021-11-22 16:38:51 +0100

    19

---
+++ orus-dapp/src/components/InfoSection/Data.js
@@ -0,0 +1,16 @@
+export const homeObjOne = {
+    id: 'about',
+    lightBg: false,
+    lightText: true,
+    lightTextDesc: true,
+    topLine: 'Premium Bank',
+    headline: 'Unlimited Transactions with zero fees',
+    description: 'Get accesss to our exclusive app that allows you to send unlimited transaction with 0 fees.',
+    buttonLabel: 'Get started',
+    imgStart: false,
+    img: require('../../images/svg-1.svg'),
+    alt: 'Car',
+    dark: true,
+    primary: true,
+    darkText: false
+};
--- orus-dapp/src/pages/index.js
+++ orus-dapp/src/pages/index.js
@@ -2,8 +2,11 @@ import React, { useState } from 'react';
 import Navbar from '../components/Navbar';
 import Sidebar from '../components/Sidebar';
 import HeroSection from '../components/HeroSection';
+import InfoSection from '../components/InfoSection';
+import { homeObjOne } from '../components/InfoSection/Data';
 
 const Home = () => {
+
     const [ isOpen, setIsOpen ] = useState(false);
 
     const toggle = () => {
@@ -15,6 +18,7 @@ const Home = () => {
             <Sidebar isOpen={isOpen} toggle={toggle} />
             <Navbar toggle={toggle} />
             <HeroSection />
+            <InfoSection {...homeObjOne}/>
         </>
     )
 }
